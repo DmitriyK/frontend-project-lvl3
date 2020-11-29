c800e7726c4f12ffdab1f139c76fcaf46883182c
Dmitriy <dima.komlyk@gmail.com>
2020-11-29 16:36:39 +0100

    Import i18next, update init function

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+import i18next from 'i18next';
 import request from './request.js';
 import watch from './watch.js';
 import validation from './validator.js';
 import parser from './parser.js';
+import resources from './locale';
 
 export default class Feed {
   constructor(form) {
@@ -12,8 +14,14 @@ export default class Feed {
       url: '',
       feeds: [],
       posts: [],
-      error: '',
+      successMessage: '',
+      errorMessage: '',
     };
+    this.i18next = i18next.init({
+      lng: 'en',
+      debug: true,
+      resources,
+    });
   }
 
   init() {
@@ -21,7 +29,6 @@ export default class Feed {
       e.preventDefault();
       const formData = new FormData(e.target);
       this.state.url = formData.get('url');
-      watch(this.state).processState = 'sending';
       const searchFeed = new Promise((resolve, reject) => {
         this.state.feeds.forEach((feed) => {
           if (feed.feedUrl === this.state.url) {
@@ -30,29 +37,33 @@ export default class Feed {
         });
         resolve(this.state.url);
       });
+      watch(this.state).processState = 'sending';
+      this.i18next.then((t) => {
       // Search for duplicates url
-      searchFeed.then((url) => {
+        searchFeed.then((url) => {
         // Validate url
-        validation(url).then(() => {
+          validation(url).then(() => {
           // Get an http request
-          request(url).then((response) => {
-            const feedId = this.state.feeds.length + 1;
-            const feedUrl = this.state.url;
-            const { feed, posts } = parser(response.data.contents, feedId, feedUrl);
-            this.state.feeds = [feed, ...this.state.feeds];
-            this.state.posts = [...posts, ...this.state.posts];
-            watch(this.state).processState = 'finished';
+            request(url).then((response) => {
+              const feedId = this.state.feeds.length + 1;
+              const feedUrl = this.state.url;
+              const { feed, posts } = parser(response.data.contents, feedId, feedUrl);
+              this.state.feeds = [feed, ...this.state.feeds];
+              this.state.posts = [...posts, ...this.state.posts];
+              this.state.successMessage = t('form.feedback.success');
+              watch(this.state).processState = 'finished';
+            }).catch(() => {
+              this.state.errorMessage = t('form.feedback.error.request');
+              watch(this.state).processState = 'failed';
+            });
           }).catch(() => {
-            this.state.error = 'This source doesn\'t contain valid rss';
+            this.state.errorMessage = t('form.feedback.error.validate');
             watch(this.state).processState = 'failed';
           });
         }).catch(() => {
-          this.state.error = 'Must be valid url';
+          this.state.errorMessage = t('form.feedback.error.exists');
           watch(this.state).processState = 'failed';
         });
-      }).catch(() => {
-        this.state.error = 'Rss already exists';
-        watch(this.state).processState = 'failed';
       });
     });
   }
