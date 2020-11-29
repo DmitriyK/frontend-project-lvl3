929608169602cfd53da11aff1b6822039d9d2009
Dmitriy <dima.komlyk@gmail.com>
2020-11-29 14:17:22 +0100

    Update file. Create a request function

--- src/request.js
+++ src/request.js
@@ -1,45 +1,5 @@
-import watchedState from './watch.js';
-// import axios from 'axios';
-import isValid from './validator.js';
+import axios from 'axios';
 
-export default class Request {
-  constructor(form) {
-    this.form = form;
-  }
+const corsUrl = 'https://api.allorigins.win/get?url=';
 
-  init() {
-    const state = {
-      rssForm: {
-        processState: '',
-        processError: null,
-        url: '',
-        valid: true,
-        errors: {},
-      },
-    };
-
-    this.form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const formData = new FormData(e.target);
-      state.rssForm.url = formData.get('url');
-      watchedState(state).rssForm.processState = 'sending';
-      try {
-        const [validState, errMessage] = isValid(state.rssForm.url);
-        if (validState) {
-          watchedState(state).rssForm.valid = validState;
-          // axios.get();
-          // fetch();
-          watchedState(state).rssForm.processState = 'finished';
-        } else {
-          watchedState(state).rssForm.valid = validState;
-          watchedState(state).rssForm.errors = errMessage;
-        }
-      } catch (err) {
-        console.log(err);
-        watchedState(state).rssForm.errors = err;
-        watchedState(state).rssForm.processState = 'failed';
-        throw err;
-      }
-    });
-  }
-}
+export default (url) => axios.get(`${corsUrl}${encodeURIComponent(url)}`);
