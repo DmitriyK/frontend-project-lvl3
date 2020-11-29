dcec42cc4ad86bc0d1d21541ae7a65105882cdc2
Dmitriy <dima.komlyk@gmail.com>
2020-11-29 13:25:44 +0100

    Building the logic of the validation function

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,8 @@
-// import * as yup from 'yup';
+import * as yup from 'yup';
 
-export default (url) => {
-  console.log(url);
-};
+const schema = yup.object().shape({
+  url: yup.string().required()
+    .matches(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/),
+});
+
+export default (url) => schema.validate({ url });
