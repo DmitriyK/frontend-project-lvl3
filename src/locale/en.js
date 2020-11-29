56eaf326e6f4479c4b069b2095dde8daf0e2092a
Dmitriy <dima.komlyk@gmail.com>
2020-11-29 16:31:10 +0100

    Creating file

---
+++ src/locale/en.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    form: {
+      feedback: {
+        success: 'Rss has been loaded',
+        error: {
+          validate: 'Must be valid url',
+          exists: 'Rss already exists',
+          request: 'This source doesn\'t contain valid rss',
+        },
+      },
+    },
+  },
+};
