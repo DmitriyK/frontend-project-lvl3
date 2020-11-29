46d8ec7b921deb3131df85fb0edb37464378a919
Dmitriy <dima.komlyk@gmail.com>
2020-11-29 14:26:52 +0100

    Update file. Building a parsing function

--- src/parser.js
+++ src/parser.js
@@ -1,3 +1,24 @@
-export default (data) => {
-  console.log(data);
+const patt = /<!\[CDATA\[(.*)\]\]>/gm;
+
+export default (data, feedId, feedUrl) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(data, 'application/xml');
+  const titleFeed = doc.querySelector('title').innerHTML.replace(patt, '$1');
+  const descriptionFeed = doc.querySelector('description').innerHTML.replace(patt, '$1');
+  const items = doc.querySelectorAll('item');
+  const posts = [];
+  items.forEach((item) => {
+    const titlePost = item.querySelector('title').innerHTML.replace(patt, '$1');
+    const linkPost = item.querySelector('link').innerHTML.replace(patt, '$1');
+    posts.push({ titlePost, linkPost, feedId });
+  });
+  return {
+    feed: {
+      titleFeed,
+      descriptionFeed,
+      feedId,
+      feedUrl,
+    },
+    posts,
+  };
 };
