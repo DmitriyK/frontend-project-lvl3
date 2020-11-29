6f0c8173984788417859d0968124b6a7f0ff7057
Dmitriy <dima.komlyk@gmail.com>
2020-11-29 14:24:41 +0100

    Update file. Building the render function.

---
+++ src/render.js
@@ -0,0 +1,55 @@
+const containerFeedback = document.querySelector('.feedback');
+
+const containerFeeds = document.querySelector('.feeds');
+
+const renderFeeds = (feeds) => {
+  const rowTitleFeeds = document.createElement('h2');
+  rowTitleFeeds.innerHTML = 'Feeds';
+  const listFeeds = document.createElement('ul');
+  listFeeds.classList.add('list-group', 'mb-5');
+  feeds.forEach((feed) => {
+    const itemFeed = document.createElement('li');
+    itemFeed.classList.add('list-group-item');
+    const titleFeed = document.createElement('h3');
+    titleFeed.innerHTML = feed.titleFeed;
+    itemFeed.append(titleFeed);
+    const descriptionFeed = document.createElement('p');
+    descriptionFeed.innerHTML = feed.descriptionFeed;
+    itemFeed.append(descriptionFeed);
+    listFeeds.append(itemFeed);
+  });
+  containerFeeds.append(rowTitleFeeds);
+  containerFeeds.append(listFeeds);
+};
+
+const renderPosts = (posts) => {
+  const rowTitlePosts = document.createElement('h2');
+  rowTitlePosts.innerHTML = 'Posts';
+  const listPosts = document.createElement('ul');
+  listPosts.classList.add('list-group');
+  posts.forEach((post) => {
+    const itemPost = document.createElement('li');
+    itemPost.classList.add('list-group-item');
+    const linkPost = document.createElement('a');
+    linkPost.setAttribute('href', `${post.linkPost}`);
+    linkPost.innerHTML = post.titlePost;
+    itemPost.append(linkPost);
+    listPosts.append(itemPost);
+  });
+  containerFeeds.append(rowTitlePosts);
+  containerFeeds.append(listPosts);
+};
+
+export const renderErrors = ({ error }) => {
+  containerFeedback.classList.add('text-danger');
+  containerFeedback.innerHTML = error;
+};
+
+export default ({ feeds, posts }) => {
+  containerFeedback.classList.remove('text-danger');
+  containerFeedback.classList.add('text-success');
+  containerFeedback.innerHTML = 'Rss has been loaded';
+  containerFeeds.innerHTML = '';
+  renderFeeds(feeds);
+  renderPosts(posts);
+};
