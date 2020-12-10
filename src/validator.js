2803c0a57ed5379683cdeebaf1d0882b13b5180f
Dmitriy <dima.komlyk@gmail.com>
2020-12-10 09:46:50 +0100

    Rename file

--- src/validator.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-const schema = yup.object().shape({
-  url: yup.string().required()
-    .matches(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/),
-});
-
-export default (url) => schema.validate({ url });
