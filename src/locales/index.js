11db99e273fcb81e44c0dacdb8453a2b78341363
Dmitriy <dima.komlyk@gmail.com>
2021-02-15 18:27:26 +0100

    refactoring v4 - add setLocale

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,3 +1,13 @@
+import { setLocale } from 'yup';
 import en from './en.js';
+
+setLocale({
+  string: {
+    url: 'invalid',
+  },
+  mixed: {
+    notOneOf: 'exists',
+  },
+});
 
 export default { en };
