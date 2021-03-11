cb7c7dac97829ef27fedf435e95785a05c7fbb5c
Dmitriy <dima.komlyk@gmail.com>
2021-03-11 17:50:46 +0100

    Update file

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import * as $ from 'jquery';
+import $ from 'jquery';
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { setLocale } from 'yup';
