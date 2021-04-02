3b12107ce2b39c1651e4c144b2a72f2b22520eb4
Dmitriy <dima.komlyk@gmail.com>
2021-04-02 16:47:47 +0300

    Update file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
